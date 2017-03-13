052441433a4674f14fc8fdeb142e0da27b84a8f8
Luisa Manitz <luisa.manitz@googlemail.com>
2017-03-13 05:20:14 +0100

    integrate service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,38 +3,43 @@ import ApiService from './services/api-service';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
-   selector: 'app-root',
-   providers: [ApiService],
-   templateUrl: './app.component.html',
-   styleUrls: ['./app.component.css'],
-   template: `
-       <h1 async>
-           {{selectedItem.name}}
-           {{data.timezone}}
-       </h1>
-       <h2>
-           JAJA!
-       </h2>
-       `
+    selector: 'app-root',
+    providers: [ApiService],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    template: `
+        <h1 async>
+            {{selectedItem.name}}
+            {{data.timezone}}
+        </h1>
+        <h2>
+            JAJA!
+        </h2>
+        `
 })
-
 export class AppComponent implements OnInit {
-   title = 'app works!';
-   hero = 'Whatever';
-   selectedItem = '';
+    title = 'app works!';
+    hero = 'Whatever';
+    selectedItem = '';
 
-   data = {};
+    data = {};
 
-   onSelect = (item) => {
-       this.selectedItem = item;
-   };
+    onSelect = (item) => {
+        this.selectedItem = item;
+    };
 
-   constructor(public apiService: ApiService){}
+    constructor(public apiService: ApiService){}
 
-   ngOnInit(): void {
-       this.apiService.getData().subscribe((data) => {
-           console.log(data);
-           this.data = data
-       })
-   }
+    ngOnInit(): void {
+        navigator.geolocation.getCurrentPosition((pos) => {
+            this.apiService.geoLocation = pos;
+            this.apiService.getData().subscribe((data) => {
+                console.log(data);
+                this.data = data
+            });
+        }, (err) => {
+            console.log(err);
+        });
+
+    }
 }
--- src/app/services/api-service.ts
+++ src/app/services/api-service.ts
@@ -7,24 +7,36 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export default class ApiService {
 
-   constructor (private jsonp: Jsonp) {}
+    constructor (private jsonp: Jsonp) {}
 
-   public test = 'huhuhuhuhu';
+    public test = 'huhuhuhuhu';
 
-   private apiUrl = 'https://api.darksky.net/forecast/f4cdaf9c94d44202f8638115338f52a4/37.8267,-122.4233';
+    public geoLocation = {
+        coords: {
+            latitude: 0,
+            longitude: 0
+        }
+    };
 
-   private extractData(res: Response) {
-       let body = res.json();
-       return body || { };
-   }
+    private apiUrl = '';
 
-   public getData = () => {
-       let params = new URLSearchParams();
-       params.set('format', 'json');
-       params.set('callback', 'JSONP_CALLBACK');
+    private extractData(res: Response) {
+        let body = res.json();
+        return body || { };
+    }
 
-       return this.jsonp
-           .get(this.apiUrl, { search: params })
-           .map(this.extractData);
-   }
+    public getData = () => {
+        let params = new URLSearchParams();
+        params.set('format', 'json');
+        params.set('callback', 'JSONP_CALLBACK');
+
+        let coord = this.geoLocation.coords;
+
+        this.apiUrl = `https://api.darksky.net/forecast/f4cdaf9c94d44202f8638115338f52a4/${coord.latitude},${coord.longitude}`;
+
+
+        return this.jsonp
+            .get(this.apiUrl, { search: params })
+            .map(this.extractData);
+    }
 }
